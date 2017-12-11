ef866f0ae5508cfa28b719434ddc6d5719c6d402
Ciro De Rosa <ciroderosa@10-20-32-69.0100.fed.it.iosda.org>
2017-12-11 09:41:59 +0100

    Initial Commit

---
+++ Chrome&KillTests/Chrome_KillTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chrome_KillTests.swift
+//  Chrome&KillTests
+//
+//  Created by Ciro De Rosa on 11/12/17.
+//  Copyright © 2017 Ciro De Rosa. All rights reserved.
+//
+
+import XCTest
+@testable import Chrome_Kill
+
+class Chrome_KillTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
